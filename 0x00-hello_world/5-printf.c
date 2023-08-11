2d9bb67b8934ba7f42c1d8f0de30ec4c301ca084
keffoh <kennedyeffoh1@gmail.com>
2023-08-10 21:14:05 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: 0 when successfully
+ */
 int main(void)
 {
-       printf("Hello);
+       printf("with proper grammar, but the outcome is a piece of art,\n");
        return (0);
 }
