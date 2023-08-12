50f747874d451d09ca7f57111f681ee930a241e1
keffoh <kennedyeffoh1@gmail.com>
2023-08-11 21:18:34 -0700

    ./5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the main function of the code
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+int num;
+while (num < 10)
+{
+printf("%d", num);
+num += 1;
+}
+putchar('\n');
+return (0);
+
+}
